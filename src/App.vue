2a17d4859f8f5904934f75099b0e607c3fbe28f5
dymafr <erwan1309@hotmail.com>
2022-02-17 11:37:43 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ getTitle() }}</h1>
+  <input :type="getType()" />
+</template>
+
+<script setup lang="ts">
+function getTitle() {
+  return 'Je suis un titre';
+}
+
+function getType() {
+  return 'number';
+}
+</script>
+
+<style></style>
